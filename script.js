750303cb3248790d00ea1acd953385b7489fb0af
hkcfied <41416378+hkcfied@users.noreply.github.com>
2023-02-24 15:35:16 +0530

    code working for a single round

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-/* getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors */
+// getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById("paperButton");
+const scissorsButton = document.getElementById("scissorsButton");
+const currentPoints = document.getElementById("currentPoints");
+const whoChoseWhat = document.getElementById("whoChoseWhat");
+const whoWon = document.getElementById("whoWon");
+const finalWinner = document.getElementById("finalWinner");
+
+rockButton.addEventListener("click", function () {
+  playRound("Rock");
+});
+paperButton.addEventListener("click", function () {
+  playRound("Paper");
+});
+scissorsButton.addEventListener("click", function () {
+  playRound("Scissors");
+});
 
 function getComputerChoice() {
   // Define an array with the three options
@@ -10,77 +27,40 @@ function getComputerChoice() {
   //Define computers choice
   const compChoice = options[randomIndex];
 
-  //alert(compChoice);
-  //console.log(playerSelection, computerSelection);
   return compChoice;
 }
 
 let playerSelection;
 let computerSelection;
 let result;
+let roundCount = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-function getPlayerChoice() {
-  //get Player Choice from HTML
-  const playerChoice = document.getElementById("playerOption").value;
-
-  //alert(playerChoice);
-  //return playerChoice;
-
-  playerSelection = playerChoice;
+function playRound(playerSelection) {
+  roundCount++;
   computerSelection = getComputerChoice();
-  //alert(playerSelection + " " + computerSelection);
-  result = playRound(playerSelection, computerSelection);
-  //alert(result);
-  if (result == 2)
-    alert(
-      `It's a Draw! You Chose ${playerSelection} and Computer also Chose ${computerSelection}`
-    );
-  else if (result == 1)
-    alert(
-      `You Win! You Chose ${playerSelection} and Computer Chose ${computerSelection}`
-    );
-  else if (result == 0)
-    console.log(
-      `You Lose! You Chose ${playerSelection} and Computer Chose ${computerSelection}`
-    );
-}
-
-function playRound(playerSelection, computerSelection) {
-  //alert("Im In");
-  if (playerSelection == computerSelection) {
-    return 2;
+  if (playerSelection === computerSelection) {
+    result = 2;
   } else if (
-    (playerSelection == "Rock" && computerSelection == "Scissors") ||
-    (playerSelection == "Paper" && computerSelection == "Rock") ||
-    (playerSelection == "Scissors" && computerSelection == "Paper")
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
-    return 1;
+    result = 1;
+    playerScore++;
   } else {
-    return 0;
+    result = 0;
+    computerScore++;
   }
+
+  updatePage(playerSelection, computerSelection);
 }
 
-const rockButton = document.getElementById("rockButton");
-const paperButton = document.getElementById("paperButton");
-const scissorsButton = document.getElementById("scissorsButton");
-const currentPoints = document.getElementById("currentPoints");
-const whoChoseWhat = document.getElementById("whoChoseWhat");
-const whoWon = document.getElementById("whoWon");
-const finalWinner = document.getElementById("finalWinner");
-
-rockButton.addEventListener("click", playRound("Rock", computerSelection));
-paperButton.addEventListener("click", playRound("Paper", computerSelection));
-scissorsButton.addEventListener(
-  "click",
-  playRound("Scissors", computerSelection)
-);
-
-/*  Write a function that plays a single round of Rock Paper Scissors. 
-The function should take two parameters - the playerSelection and computerSelection
- - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock */
-
-/*
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-*/
+function updatePage(playerSelection, computerSelection) {
+  if (result == 2) whoWon.textContent = `It's a Draw!`;
+  else if (result == 1)
+    whoWon.textContent = `You Win! You Chose ${playerSelection} and Computer Chose ${computerSelection}`;
+  else if (result == 0)
+    whoWon.textContent = `You Lose! You Chose ${playerSelection} and Computer Chose ${computerSelection}`;
+}
