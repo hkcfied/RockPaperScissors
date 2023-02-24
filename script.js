3fb212e743576c8e396b3d5acb15116f530fa3bb
hkcfied <41416378+hkcfied@users.noreply.github.com>
2023-02-24 09:43:33 +0530

    Add Rock, Paper, Scissor function

--- script.js
+++ script.js
@@ -11,12 +11,28 @@ function getComputerChoice() {
   const compChoice = options[randomIndex];
 
   //alert(compChoice);
-
+  //console.log(playerSelection, computerSelection);
   return compChoice;
 }
 
 let playerSelection;
-const computerSelection = getComputerChoice();
+let computerSelection;
+let result;
+
+function playRound(playerSelection, computerSelection) {
+  //alert("Im In");
+  if (playerSelection == computerSelection) return 2;
+  else if (playerSelection == "Rock" && computerSelection == "Paper") return 0;
+  else if (playerSelection == "Rock" && computerSelection == "Scissors")
+    return 1;
+  else if (playerSelection == "Paper" && computerSelection == "Scissors")
+    return 0;
+  else if (playerSelection == "Paper" && computerSelection == "Rock") return 1;
+  else if (playerSelection == "Scissors" && computerSelection == "Rock")
+    return 0;
+  else if (playerSelection == "Scissors" && computerSelection == "Paper")
+    return 1;
+}
 
 function getPlayerChoice() {
   //get Player Choice from HTML
@@ -24,20 +40,38 @@ function getPlayerChoice() {
 
   //alert(playerChoice);
   //return playerChoice;
+
   playerSelection = playerChoice;
+  computerSelection = getComputerChoice();
+  //alert(playerSelection + " " + computerSelection);
+  result = playRound(playerSelection, computerSelection);
+  //alert(result);
+  if (result == 2)
+    alert(
+      "It's a Draw! You Chose " +
+        playerSelection +
+        " and Computer also Chose " +
+        computerSelection
+    );
+  else if (result == 1)
+    alert(
+      "You Win! You Chose " +
+        playerSelection +
+        " and Computer Chose " +
+        computerSelection
+    );
+  else if (result == 0)
+    alert(
+      "You Lose! You Chose " +
+        playerSelection +
+        " and Computer Chose " +
+        computerSelection
+    );
 }
 
 /*  Write a function that plays a single round of Rock Paper Scissors. 
 The function should take two parameters - the playerSelection and computerSelection
  - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock */
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "Rock" && computerSelection == "Scissors")
-    return "Win";
-  else if (playerSelection == "Rock" && computerSelection == "Paper") return 1;
-  else if (playerSelection == "Rock" && computerSelection == "Scissors")
-    return 1;
-}
 
 /*
 const playerSelection = "rock";
